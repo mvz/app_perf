de66446e087774f55bcab7fd699b9ebaf853ea00
Matijs van Zuijlen <matijs@matijs.net>
2018-06-30 16:14:51 +0200

    Make host setting explicit

--- config/initializers/app_perf_rpm.rb
+++ config/initializers/app_perf_rpm.rb
@@ -3,6 +3,7 @@ require 'app_perf_rpm'
 AppPerfRpm.configure do |rpm|
   rpm.application_name = "App Perf"
   rpm.license_key = ENV["APP_PERF_LICENSE_KEY"]
+  rpm.host = ENV["APP_PERF_HOST"]
   rpm.ssl = ENV["APP_PERF_SSL"] == "true"
   rpm.sample_rate = ENV["APP_PERF_SAMPLE_RATE"].to_i
   rpm.flush_interval = ENV["APP_PERF_FLUSH_INTERVAL"].to_i
