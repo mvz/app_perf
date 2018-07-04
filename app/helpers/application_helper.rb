58e7d3069a07e477a8f3da57f1ee99a11e60269e
Matijs van Zuijlen <matijs@matijs.net>
2018-07-04 18:06:08 +0200

    Fix format calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -114,7 +114,7 @@ module ApplicationHelper
     rgb[0] = (rgb[0].to_i * amount).round
     rgb[1] = (rgb[1].to_i * amount).round
     rgb[2] = (rgb[2].to_i * amount).round
-    format('%02x%02x%02x', rgb)
+    format('%02x%02x%02x', *rgb)
   end
 
   def lighten_hex(hex_color, amount = 0.4)
@@ -123,6 +123,6 @@ module ApplicationHelper
     rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
     rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
     rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
-    format('%02x%02x%02x', rgb)
+    format('%02x%02x%02x', *rgb)
   end
 end
